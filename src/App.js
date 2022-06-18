02fc7f88c0689b017385475abab088747cf543bd
talib <imonn439@gmail.com>
2022-06-18 16:18:58 +0530

    working on main layouts

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import Home from './Pages/Home';
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import PropDetailFull from './Pages/PropDetailFull'
@@ -16,8 +17,24 @@ import Dashboard from './Pages/Dashboard';
 import TableData from './Components/TableData';
 import DocumentView from './Components/DocumentView'
 import DocumentViewPage from './Pages/DocumentViewPage';
+import { connect } from "react-redux";
 
-function App() {
+
+function App(props) {
+  const [boxWidth, setBoxWidth] = useState({width:'md:w-4/5',margin:'md:ml-64'})
+  useEffect(() => {
+   if(props.navOriginalCloseStatus==true){
+     setBoxWidth({width:'md:w-full',margin:'md:ml-0'})
+     console.log(boxWidth.width," ",boxWidth.margin)
+   }else{
+    setBoxWidth({width:'md:w-4/5',margin:'md:ml-64'})
+    console.log(boxWidth.width," ",boxWidth.margin)
+
+   }
+  
+   
+  }, [props.navOriginalCloseStatus])
+  
   return (
     <>
       {/* <Home /> */}
@@ -25,7 +42,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Sidebar />
-        <div className="sm:w-full md:w-4/5 sm:ml-0 md:ml-64 mt-2">
+        <div className={"sm:w-full transition-all pl-12 "+boxWidth.width+ " sm:ml-0 "+boxWidth.margin +" mt-2"}>
+        {/* <div className={"sm:w-full "+"md:w-4/5"+ " sm:ml-0 "+"md:ml-64" +" mt-2"}> */}
           <Routes>
             <Route path='/' element={<DataGrid2 />} />
             <Route path='/prop-dtl' element={<PropDetailFull />} />
@@ -40,4 +58,21 @@ function App() {
   );
 }
 
-export default App;
+//for redux
+const mapStateToProps = (state) => {
+  return {
+    navCloseStatus: state.navCloseStatus,
+    navOriginalCloseStatus: state.navOriginalCloseStatus
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // buyCake: () => dispatch(buyCake())
+    NAV_OPEN: (data2) => dispatch({ type: "NAV_OPEN" }),
+    NAV_CLOSE: (data3) => dispatch({ type: "NAV_CLOSE" })
+  };
+};
+// export default Home // EXPORTING HOME COMPONENT
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// export default App;
